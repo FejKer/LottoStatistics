5095b7e2f3916433caee4a4efd57cf623019873e
Oskar Migowski <o.migowski@protonmail.com>
2022-11-12 08:53:25 +0100

    Optimized imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,6 @@ module ovh.fejker.lottostatistics {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
     requires org.jsoup;
     requires org.json;
     requires org.apache.commons.io;
